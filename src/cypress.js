fac8b64bf31331f093575e351d0615e0a625cfdc
Kylee Tilley <kyleetilley@gmail.com>
2019-11-21 18:44:29 -0600

    Remove vendor page objects  until supported

--- src/cypress.js
+++
@@ -1,7 +0,0 @@
-import PageObject from "./pageObject";
-
-module.exports = class CypressPageObject extends PageObject {
-  constructor(driver, root) {
-    super(driver, root, "find", "filter");
-  }
-};
--- src/testinglib.js
+++
@@ -1,7 +0,0 @@
-import PageObject from "./pageObject";
-
-module.exports = class TestingLibPageObject extends PageObject {
-  constructor(driver, root) {
-    super(driver, root, "queryBy", "queryAllBy");
-  }
-};
